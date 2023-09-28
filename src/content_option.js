9405dd660b32331003deacd057dafd4049343a60
Matt MacFarlane <tidydata@outlook.com>
2023-09-28 04:57:56 -0400

    Made it Alina focused

--- src/content_option.js
+++ src/content_option.js
@@ -1,15 +1,15 @@
-const logotext = "JOHN";
+const logotext = "ALINA";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Alina Zelenska",
+    description: "I’m Alina Zelenska _ Ukrainian / Russian / English language tutor _ Poet & Writer _ currently working in Poland",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Alina Zelenska",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        first: "I love helping people learn languages",
+        second: "I write multilingual poems",
+        third: "I consult on language learning apps",
     },
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
